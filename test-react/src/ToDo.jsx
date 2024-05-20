4a7945e4e78df3d4a61cec48fdb2542444e948de
Andei <na1x.1024@gmail.com>
2024-05-20 19:19:35 +0300

    feat: changed complete/declene button mechanic

--- test-react/src/ToDo.jsx
+++ test-react/src/ToDo.jsx
@@ -7,9 +7,9 @@ const ToDo = (props) => {
                 <p><strong className="strong">{props.todoo.title}</strong> - {props.todoo.description}</p>
                 <p className="list-elem-descr">Completed: {props.todoo.done ? "Done" : "Not done"}</p>
                 <div className="buttons">
-                    <button className={!props.todoo.done ? 'show-elem btn btn-success' : 'hide-elem btn-success'} onClick={() => props.editDone(props.todoo, true)}>Confirm
-                    </button>
-                    <button className={props.todoo.done ? 'show-elem btn btn-warning' : 'hide-elem btn-warning'} onClick={() => props.editDone(props.todoo, false)}>Decline</button>
+
+                    <button className={!props.todoo.done ? 'btn btn-success' : 'btn btn-warning'} onClick={!props.todoo.done ? () => props.editDone(props.todoo, true) : () => props.editDone(props.todoo, false)}>{!props.todoo.done ? "Confirm" : "Decline"}</button>
+
                     <button className="btn btn-danger ${props.todoo.done ? 'show-elem' : 'hide-elem'}"  onClick={() => props.removeToDo(props.todoo)}>Delete
                     </button>
                 </div>
